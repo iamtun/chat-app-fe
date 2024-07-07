fb388a86031b3a8a5f3fcfe46894b5313a4df555
iamtun <letuan19431791.iuh@gmail.com>
2024-07-07 14:38:40 +0700

    feat(auth): config alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -9,6 +10,11 @@ export default defineConfig(({mode}) => {
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     server: {
       port: +PORT,
       host: HOST,
